58d256207e13c5a3be3a31bbc6439aaab9140009
viktorstrate <viktorstrate@gmail.com>
2020-01-26 14:57:37 +0100

    Basic example is now working

--- client/client.go
+++ client/client.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/hajimehoshi/oto"
 )
@@ -27,6 +28,22 @@ func main() {
 
 	fmt.Printf("Samplerate: %d\n", sampleRate)
 
+	startStr := resp.Header.Get("X-Start-Time")
+	if len(startStr) == 0 {
+		panic("Start-Time header was not set")
+	}
+	startTimeRaw, err := strconv.Atoi(startStr)
+	if err != nil {
+		chk(err)
+	}
+	startTimeSec := int64(time.Duration(startTimeRaw) / time.Second)
+	startTime := time.Unix(startTimeSec, int64(startTimeRaw)-startTimeSec*int64(time.Second))
+
+	startDelay := startTime.Sub(time.Now())
+	startTimer := time.NewTimer(startDelay)
+	fmt.Printf("Start delay %d ms", startDelay/time.Millisecond)
+
+	<-startTimer.C
 	p, err := oto.NewPlayer(sampleRate, 2, 2, 8192)
 	if err != nil {
 		chk(err)
--- server/server.go
+++ server/server.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"sync"
@@ -12,14 +10,14 @@ import (
 	"github.com/hajimehoshi/go-mp3"
 )
 
-const sampleRate = 44100
-const seconds = 1
+const CHUNK_SIZE = 4608
+const TIME_DELAY = 2 * time.Second
 
 type SongData struct {
-	buffer    bytes.Buffer
-	decoder   mp3.Decoder
-	timestamp time.Time
-	lock      sync.Mutex
+	decoder          mp3.Decoder
+	timestamp        time.Time
+	timestamp_update time.Time
+	lock             sync.Mutex
 }
 
 func loadSongData() *SongData {
@@ -27,27 +25,58 @@ func loadSongData() *SongData {
 	if err != nil {
 		chk(err)
 	}
-	defer f.Close()
 
 	mp3decoder, err := mp3.NewDecoder(f)
-	var mp3buffer bytes.Buffer
-
-	fmt.Println("Filling mp3 buffer")
-
-	io.Copy(&mp3buffer, mp3decoder)
 
 	song_data := SongData{
-		buffer:    mp3buffer,
-		decoder:   *mp3decoder,
-		timestamp: time.Now(),
+		decoder:          *mp3decoder,
+		timestamp:        time.Now(),
+		timestamp_update: time.Now(),
 	}
 
 	return &song_data
 }
 
+func start_streamer(song_data *SongData, data_channels *[]chan []byte, decode_mutex *sync.Mutex) {
+	fmt.Println("Starting streaming")
+
+	for true {
+		for song_data.timestamp.Sub(time.Now()) < 0 {
+			decode_mutex.Lock()
+
+			// Send chunk
+			chunk := make([]byte, CHUNK_SIZE)
+			bytes, err := song_data.decoder.Read(chunk)
+			if err != nil {
+				chk(err)
+			}
+
+			for _, client := range *data_channels {
+				client <- chunk
+			}
+
+			var duration time.Duration = time.Duration(float64(time.Second) * float64(bytes) / float64(song_data.decoder.SampleRate()) / 4.0)
+
+			// fmt.Printf("Read %d bytes, duration %d ms\n", bytes, duration/time.Millisecond)
+			song_data.timestamp = song_data.timestamp.Add(duration)
+			song_data.timestamp_update = time.Now()
+
+			decode_mutex.Unlock()
+		}
+
+		wait_timer := time.NewTimer(song_data.timestamp.Sub(time.Now()))
+		<-wait_timer.C
+	}
+}
+
 func main() {
 
 	song_data := loadSongData()
+
+	data_channels := make([]chan []byte, 0, CHUNK_SIZE)
+	var decode_mutex sync.Mutex
+
+	go start_streamer(song_data, &data_channels, &decode_mutex)
 
 	http.HandleFunc("/audio", func(w http.ResponseWriter, r *http.Request) {
 
@@ -55,14 +84,63 @@ func main() {
 
 		w.Header().Set("Connection", "Keep-Alive")
 		w.Header().Set("Transfer-Encoding", "chunked")
+
+		// Lock to initialize client
+		decode_mutex.Lock()
+
 		w.Header().Set("X-Samplerate", fmt.Sprint(song_data.decoder.SampleRate()))
-		w.Header().Set("X-Start-Time", "100")
+
+		timeDrift := time.Now().Sub(song_data.timestamp_update)
+		fmt.Printf("Time drift: %d", timeDrift)
+
+		startTime := song_data.timestamp
+		startTime = startTime.Add(TIME_DELAY)
+		startTime = startTime.Add(timeDrift)
+
+		w.Header().Set("X-Start-Time", fmt.Sprint(startTime.UnixNano()))
+
+		client_chan := make(chan []byte)
+		data_channels = append(data_channels, client_chan)
+
+		decode_mutex.Unlock()
+
+		defer func() {
+			decode_mutex.Lock()
+			defer decode_mutex.Unlock()
+
+			index := -1
+			for i, channel := range data_channels {
+				if channel == client_chan {
+					index = i
+					break
+				}
+			}
+
+			if index == -1 {
+				fmt.Println("ERROR: Disconnecting client's channel not found")
+				return
+			}
+
+			// Remove channel from array
+			data_channels[index] = data_channels[len(data_channels)-1]
+			data_channels = data_channels[:len(data_channels)-1]
+
+			fmt.Println("Client disconnect cleanup succeeded")
+
+		}()
+
+		for true {
+			chunk := <-client_chan
+			_, err := w.Write(chunk)
+			if err != nil {
+				fmt.Println("Could not write chunk to client:")
+				fmt.Println(err.Error())
+				fmt.Println("Closing connection")
+				break
+			}
+		}
 
 		fmt.Println("Stream ended")
-
-	})
-
-	http.HandleFunc("/samplerate", func(w http.ResponseWriter, r *http.Request) {
 
 	})
 
